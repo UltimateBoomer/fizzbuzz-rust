960d369705ac379257d6f3c63afeb73fe22388ec
UltimateBoomer <caohc2017@gmail.com>
2022-12-24 20:10:12 -0500

    Dereference in closure

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -2,7 +2,7 @@ use std::collections::BTreeMap;
 
 pub fn list(start: i32, end: i32, word_map: &BTreeMap<i32, &str>) -> Vec<String> {
     (start..=end).into_iter().map(|i| {
-        word_map.into_iter().rev().find(|w| i % w.0 == 0)
+        word_map.into_iter().rev().find(|&w| i % w.0 == 0)
             .map_or(i.to_string(), |w| String::from(*w.1))
     }).collect()
 }
