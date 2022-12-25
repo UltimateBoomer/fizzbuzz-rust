b76321b568f6341f462cad7700a520f5280a04a5
UltimateBoomer <caohc2017@gmail.com>
2022-12-24 19:55:47 -0500

    Base

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -5,4 +5,4 @@ pub fn list(start: i32, end: i32, word_map: &BTreeMap<i32, &str>) -> Vec<String>
         word_map.into_iter().rev().find(|w| i % w.0 == 0)
             .map_or(i.to_string(), |w| String::from(*w.1))
     }).collect()
-}   
+}
